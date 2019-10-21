fd0f5bbc7a72d5b276811b8d80f6f01b9f70c3ae
masonkn <masonandmckay@gmail.com>
2019-10-21 16:45:45 -0600

    Initial Commit
    Got grid and block to spawn

---
+++ Packing Game/Assets/Scripts/Board.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private GameObject[,] allTiles;
+    private Vector2 tempPosition;
+
+
+    public GameObject tromino;
+    public int width = 10;
+    public int height = 16;
+    
+    void Start()
+    {
+        allTiles = new GameObject[width, height];
+        allTiles[0, height-1] = tromino;
+        GameObject square = Instantiate(tromino, new Vector2(0, height-1), Quaternion.identity);
+    }
+}
