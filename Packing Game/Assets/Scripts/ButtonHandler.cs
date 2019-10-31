66b393ade03bcb40df67b68feebfcebdee600326
masonkn <masonandmckay@gmail.com>
2019-10-30 20:58:42 -0600

    Added Two UI Scenes (Main Menus)
    Main Menu
    -Play
    -Settings
    -Quit
    Settings
    -Easy
    -Medium
    -Hard
    All the buttons work, although settings don't change after pressing easy, medium, or hard yet.

---
+++ Packing Game/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("ReplacementScene", LoadSceneMode.Single);
+    }
+    public void SettingsMenu()
+    {
+        SceneManager.LoadScene("Settings", LoadSceneMode.Single);
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+    //TODO: Create methods that influence difficulty in three modes (Maybe by changing the speed of cubes).
+}
