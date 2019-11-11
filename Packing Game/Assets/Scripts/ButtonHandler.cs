56b59dc8e867077ed979a2ef2f6b38306b9c8056
masonkn <masonandmckay@gmail.com>
2019-11-11 15:15:29 -0700

    Fixed things! Main menu working.

--- Packing Game/Assets/Scripts/ButtonHandler.cs
+++ Packing Game/Assets/Scripts/ButtonHandler.cs
@@ -8,7 +8,7 @@ public class ButtonHandler : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("ReplacementScene", LoadSceneMode.Single);
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
     }
     public void SettingsMenu()
     {
@@ -21,6 +21,14 @@ public class ButtonHandler : MonoBehaviour
     public void EndGame()
     {
         Application.Quit();
+    }
+    public void PowerUp()
+    {
+        //Does nothing right now.
+    }
+    public void Pause()
+    {
+
     }
 
     //TODO: Create methods that influence difficulty in three modes (Maybe by changing the speed of cubes).
