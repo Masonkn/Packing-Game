a642a410351f9d43f2d33444aa5c61bd2ba875a9
maeserhp96 <55401732+maeserhp96@users.noreply.github.com>
2019-11-17 16:44:57 -0700

    The game now keeps track of your total score.
    Even if you close the game and restart your score will still be saved. If you want to reset your score press "R"

---
+++ Packing Game/Assets/ScoreDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+
+    public Text totalScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalScore.text = PlayerPrefs.GetInt("TotalMoney", 0).ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+    }
+
+    public static void UpdateScore(int score)
+    {
+        score = score + PlayerPrefs.GetInt("TotalMoney", 0);
+        PlayerPrefs.SetInt("TotalMoney", score);
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.DeleteKey("TotalMoney");
+        totalScore.text = "0";
+    }
+}
--- Packing Game/Assets/Scripts/InputManager.cs
+++ Packing Game/Assets/Scripts/InputManager.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
     [HideInInspector]public Block[] activeBlocks;
     public Board board;
+    
 
     private void Start()
     {
@@ -23,6 +25,10 @@ public class InputManager : MonoBehaviour
             if (!board.isFilled())
             {
                 board.SpawnNewPiece();//And replacing it
+            }
+            else
+            {
+                ScoreDisplay.UpdateScore(board.score);
             }
         }
     }
