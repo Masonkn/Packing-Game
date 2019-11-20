7f84d903fce31d8cf4f1bf8a3e419164b26ffec0
curtmister25 <me@cutisjensen.com>
2019-11-20 15:43:48 -0700

    Added the most basic tutorial

---
+++ Packing Game/Assets/Scripts/TutorialText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialText : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
