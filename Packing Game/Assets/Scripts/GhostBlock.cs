cf127218e641427f975fafd3ee8e9125fc0692a2
maeserhp96 <55401732+maeserhp96@users.noreply.github.com>
2019-12-16 15:18:34 -0700

    Sprite randomizer

--- Packing Game/Assets/Scripts/GhostBlock.cs
+++ Packing Game/Assets/Scripts/GhostBlock.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class GhostBlock : MonoBehaviour
 {
+    private int column;
+    private int row;
+    private Board board;
 
-    private Block block;
-
-    void Awake()
+    public GhostBlock(int column, int row, Board board)
     {
-        //board = FindObjectOfType<Board>();//Since the piece is a prefab, public variables won't work and it needs to find the board once it enters the scene.  Hey, thanks for reading this long comment! :)
-        block = FindObjectOfType<Block>();
+        this.column = column;
+        this.row = row;
+        this.board = board;
+        board.gameGrid[column, row] = this.gameObject;//Putting the piece in the (mostly) top, left, center of the array
+        //this.gameObject = Instantiate(board.tromino, new Vector2(x, y), Quaternion.identity);//putting the piece in the top left of the screen
+        //block.GetComponent<Block>().movingDelay = board.movingDelay;//Telling the piece what the moving delay is currently
+        //block.layer = 2;
+        //inputManager.activeBlocks[spot] = block.GetComponent<Block>();
     }
+
+    //private Block block;
+
+    //void Awake()
+    //{
+    //    //board = FindObjectOfType<Board>();//Since the piece is a prefab, public variables won't work and it needs to find the board once it enters the scene.  Hey, thanks for reading this long comment! :)
+    //    block = FindObjectOfType<Block>();
+    //}
+    void Start()
+    {
+        //GameObject.Instantiate(this.gameObject, new Vector2(column, FindGhostDifferenceInt(board, column)), Quaternion.identity, this.gameObject.transform);
+
+    }
+
     private void Update()
     {
         if (Input.GetButtonDown("Submit") && Board.isInputEnabled)
